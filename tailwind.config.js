6e06c5236464074b59be6dddc6a8027434f3ef04
Andika Alakate <andikaalakate@gmail.com>
2023-12-22 00:15:58 +0700

    Selesai sampai Section Portfolio

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,31 @@
 module.exports = {
   content: ["./public/**/*.{html,js}", "./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        primary: '#026885',
+        dark: '#0f172a',
+        secondary: '#003f53',
+        dark_sec: '#64748b',
+      },
+      fontFamily: {
+        poppins: ['Poppins'],
+        firaMono: ['Fira Mono'],
+        firaSans: ['Fira Sans'],
+        firaCode: ['Fira Code'],
+      },
+      screens: {
+        'tablet-l': '992px',
+        'hp': '576px',
+        'hmin': '425px',
+        'minni': '375px',
+        'mini': '320px',
+      },
+    },
   },
   plugins: [],
 }
